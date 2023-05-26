19142b40492c373fd452fec100d433015579933e
murirop <panashemuriro2023@u.northwestern.edu>
2023-05-26 12:11:24 -0500

    Added category icons

--- frontend/diskounts/src/App.jsx
+++ frontend/diskounts/src/App.jsx
@@ -2,18 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import category_and_links from '../../../data/link_categories'
 
 function App() {
 
+  console.log(category_and_links)
   
 
 
-
+  
 
   return (
     <div style={{ width: '100vw', height: "100vh" }}>
       <div style={{display:'grid', gridTemplateColumns: "1fr 1fr", gridGap: 20}}>
-      <CategoryCard />
+        {/* {Object.values(category_and_links).map} */}
+      <CategoryCard icon/>
       <CategoryCard />
       <CategoryCard />
       <CategoryCard />
@@ -26,9 +29,9 @@ function App() {
 }
 
 //  has an icon and a color
-const CategoryCard = () => {
+const CategoryCard = ({icon}) => {
   return <div style={{ height: 100, width: "100%", display: 'flex', justifyContent: "center", flexDirection:"column", alignItems: "center", backgroundColor: "#F6E1C3", borderRadius: 10 }}>
-    <img src="/fashion.png" width="50" height="50"></img>
+    <img src={`/${icon}.png`} width="50" height="50"></img>
     <div>Fashion</div>
   </div>
 }
